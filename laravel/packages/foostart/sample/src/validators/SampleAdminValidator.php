ec8da23c6135f37662d8c3137d3b28d0593159fc
lehuybao <lehuybao932@gmail.com>
2017-03-01 11:04:28 +0700

    update lại validator

--- laravel/packages/foostart/sample/src/validators/SampleAdminValidator.php
+++ laravel/packages/foostart/sample/src/validators/SampleAdminValidator.php
@@ -1,24 +1,21 @@
 <?php
+
 namespace Foostart\Sample\Validators;
 
 use Event;
 use \LaravelAcl\Library\Validators\AbstractValidator;
-
 use Illuminate\Support\MessageBag as MessageBag;
 
-class SampleAdminValidator extends AbstractValidator
-{
+class SampleAdminValidator extends AbstractValidator {
+
     protected static $rules = array(
         'sample_name' => 'required',
     );
-
     protected static $messages = [];
 
-
-    public function __construct()
-    {
-        Event::listen('validating', function($input)
-        {
+    public function __construct() {
+        Event::listen('validating', function($input) {
+            
         });
         $this->messages();
     }
@@ -27,16 +24,15 @@ class SampleAdminValidator extends AbstractValidator
 
         $flag = parent::validate($input);
 
-        $this->errors = $this->errors?$this->errors:new MessageBag();
+        $this->errors = $this->errors ? $this->errors : new MessageBag();
 
-        $flag = $this->isValidTitle($input)?$flag:FALSE;
+        $flag = $this->isValidTitle($input) ? $flag : FALSE;
         return $flag;
     }
 
-
     public function messages() {
         self::$messages = [
-            'required' => ':attribute '.trans('sample::sample_admin.required')
+            'required' => ':attribute ' . trans('sample::sample_admin.required')
         ];
     }
 
@@ -50,6 +46,11 @@ class SampleAdminValidator extends AbstractValidator
         $sample_name = @$input['sample_name'];
 
 
+        if ((strlen($sample_name) <= $min_lenght) || ((strlen($sample_name) >= $max_lenght))) {
+            $this->errors->add('name_unvalid_length', trans('name_unvalid_length', ['NAME_MIN_LENGTH' => $min_lenght, 'NAME_MAX_LENGTH' => $max_lenght]));
+            $flag = TRUE;
+        }
         return $flag;
     }
+
 }
